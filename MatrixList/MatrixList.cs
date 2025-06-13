704bee4eafa3df57f329be263511381f6c2751f8
Johan Sonesson <johan@traintech.eu>
2025-06-13 17:48:41 +0100

    Add project files.

---
+++ MatrixList/MatrixList.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace MatrixList
+{
+    public class MatrixList : ListView
+    {
+        public MatrixList()
+        {
+            DoubleBuffered = true;
+        }
+    }
+}
